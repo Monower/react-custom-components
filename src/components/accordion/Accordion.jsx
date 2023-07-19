c601714410a11801f60bc42adc4026a422c4c578
monower <sadafahmed720@gmail.com>
2023-07-20 00:19:25 +0600

    working on accordion

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -5,17 +5,23 @@ import { useRef,useState } from 'react';
 
 
 export default function Accordion(prop) {
+    const [acc, setAcc] = useState(null);
+    const [clicked, setClicked] = useState(false);
+    const contentEl = useRef();
+    const HandleClick = () => {
+        setClicked(!clicked);
+    }
     return <>
         <ul>
             {
                 prop.data.map((item,index) => (
-                    <li key={index}>
-                        <button>
+                    <li onClick={HandleClick} className='overflow-hidden' key={index}>
+                        <button className='bg-gray-500'>
                             {item.question}
                             <span>-</span>
                         </button>
-                        <div>
-                            <div></div>
+                        <div ref={contentEl} className='transition-all duration-700'>
+                            <div className='max-h-4 lg:max-h-0'>{ item.answer}</div>
                         </div>
                     </li>
                 ))
